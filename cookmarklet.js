65399e579a4a31c0f5306e5d2b1044071f1214c7
TopG916 <137461829+TopG916@users.noreply.github.com>
2024-02-27 20:42:42 -0500

    Create cookmarklet.js

---
+++ cookmarklet.js
@@ -0,0 +1,3 @@
+var scriptTag = document.createElement('script');
+scriptTag.setAttribute('src', 'https://rawgit.com/vbachevhx/cookie-bookmarklet/master/dist/cookie_bookmarklet.js');
+document.body.appendChild(scriptTag)
