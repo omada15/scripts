ff7f6e48b4d787ebddaa859917cc00a63f9899ea
TopG916 <137461829+TopG916@users.noreply.github.com>
2024-02-27 20:36:15 -0500

    Create edit.js

---
+++ edit.js
@@ -0,0 +1,7 @@
+if(document.body.contentEditable !== 'true') {
+  document.body.contentEditable = 'true'; 
+  document.designMode='on'; void 0
+} else {    
+  document.body.contentEditable = 'false'; 
+  document.designMode='off'; void 0
+}
