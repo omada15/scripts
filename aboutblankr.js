1e4beb84351c447609d2185ae4d332871621b4eb
TopG916 <137461829+TopG916@users.noreply.github.com>
2024-03-08 20:57:33 -0500

    Update aboutblankr.js

--- aboutblankr.js
+++ aboutblankr.js
@@ -1,12 +1,12 @@
-var url = prompt("Paste the link you want to be embedded into an about:blank page.", "https://"); 
+var url = "https://quantomphysics.sciencesimplified.pro/"
 var urlObj = new window.URL(window.location.href); 
 win = window.open(); win.document.body.style.margin = "0"; 
 win.document.body.style.height = "100vh"; 
 var iframe = win.document.createElement("iframe"); 
-iframe.style.border = "none"; iframe.style.width = "100%"; 
-iframe.style.height = "100%"; iframe.style.margin = "0"; 
+iframe.style.border = "none"; 
+iframe.style.width = "100%"; 
+iframe.style.height = "100%"; 
+iframe.style.margin = "0"; 
 iframe.referrerpolicy = "no-referrer"; iframe.allow = "fullscreen"; 
-iframe.src = url.toString(); win.document.body.appendChild(iframe); 
-var script = win.document.createElement("script"); 
-script.src = "https://3kh0.github.io/js/main.js"; 
-win.document.body.appendChild(script);
+iframe.src = url.toString(); 
+win.document.body.appendChild(iframe); 
