7b3cd1561874028eb2c7feae4b6614091543b7ad
TopG916 <137461829+TopG916@users.noreply.github.com>
2024-03-29 16:50:29 -0400

    Delete aboutblanker/launch.js

--- aboutblanker/launch.js
+++
@@ -1,2 +0,0 @@
-var popup = window.open("", "_blank", "width=500,height=300");
-fetch("https://raw.githubusercontent.com/TopG916/for-urun/main/aboutblanker/blank.html").then(r => r.text()).then(r => popup.document.write(r))
