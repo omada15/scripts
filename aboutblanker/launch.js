3779f3e5d711043499099957243536dc41b38ac1
TopG916 <137461829+TopG916@users.noreply.github.com>
2024-09-06 15:43:20 -0400

    Update launch.js

--- aboutblanker/launch.js
+++ aboutblanker/launch.js
@@ -1,2 +1,2 @@
-var popup = window.open("", "_blank", "width=500,height=300");
+var popup = window.open("", "_blank", "width=500,height=500");
 popup.document.write("<script> fetch('https://topg916.github.io/scripts/aboutblanker/blank.html').then(r => r.text()).then(r => document.write(r)) </script>")
