04f55be7713d3b844399c7bdfd9f0e6c590e8911
TopG916 <137461829+TopG916@users.noreply.github.com>
2024-09-17 07:30:28 -0400

    Update kill.js

--- scripts/kill.js
+++ scripts/kill.js
@@ -1,5 +1,5 @@
 /// kill.js
 for (let l = 0; l < 199; l++) {
   document.cookie = `cd${l}=${encodeURIComponent(btoa(String.fromCharCode.apply(0,crypto.getRandomValues(new Uint8Array(3168))))).substring(0,3168)} ;expires=Tue 31 July 2029 23:59:59 UTC; path=/`;
-  alert("done");
 }
+alert("done");
