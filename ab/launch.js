979da80c9ed6e63d794577f2e57dea92fe1da2ea
Omada <137461829+omada15@users.noreply.github.com>
2024-11-02 16:50:59 -0400

    Update launch.js

--- ab/launch.js
+++ ab/launch.js
@@ -1,2 +1,2 @@
 var popup = window.open("", "_blank", "width=500,height=500");
-popup.document.write("<script> fetch('https://topg916.github.io/scripts/aboutblanker/blank.html').then(r => r.text()).then(r => document.write(r)) </script>")
+popup.document.write("<script> fetch('https://omada15.github.io/scripts/ab/blank.html').then(r => r.text()).then(r => document.write(r)) </script>")
